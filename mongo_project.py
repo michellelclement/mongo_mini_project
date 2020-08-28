ff0760b9e89f0ce0be65dd4e359932f5bd2a0b71
Michelle Clement <michellelclement@gmail.com>
2020-08-28 08:14:49 +0000

    With working find functionality

--- mongo_project.py
+++ mongo_project.py
@@ -17,6 +17,25 @@ def mongo_connect(url):
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
+
+# Reusable helper function - doesn't do anything till we add find/delete and update functions
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+    
+    if not doc:
+        print("")
+        print("Error! No results found.")
+    
+    return doc
+
+
 def show_menu():
     print("")
     print("1. Add a record")
@@ -28,14 +47,43 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
+# Adding add_record function
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob, 'gender': gender, 'hair_colour': hair_colour, 'occupation': occupation, 'nationality': nationality}
+    
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+# Find record function
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
 # Call menu every time we come back to it
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            add_record()
         elif option == "2":
-            print("You have selected option 2")
+            find_record()
         elif option == "3":
             print("You have selected option 3")
         elif option == "4":
@@ -47,9 +95,9 @@ def main_loop():
             print("Invalid option")
         print("")
 
-#Call mongo connection, then create mongo collection
+#Call mongo connection, then create mongo collection - same as previous project
 conn = mongo_connect(MONGODB_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-#Display menu and process options
+#Display menu and process options - same as previous projects
 main_loop()
